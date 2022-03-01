991eaab0b04d19fcd062e0dac0233beb1d5544ab
Ewerton Nunes - Global <ewerton.florencio@outlook.com>
2022-03-01 02:24:31 -0300

    Working V2

--- functions/content.c
+++ functions/content.c
@@ -15,11 +15,14 @@ int	main()
 	printf("\t\t<title>Form Answer</title>\n");
 	printf("\t</head>\n");
 	printf("\t<body>\n");
+
 	printf("\t\t<h1>Resultado!</h1>\n");
 	printf("\t\t<h1>Word é: %s</h1>\n", word);
     printf("\t\t<h1>String é: %s</h1>\n", string);
+
 	printf("\t</body>\n");
-	printf("</html>\n");
+    printf("</html>\n");
 	printf("\n\n");
+
 	return 0;
 }
