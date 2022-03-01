e8ce9bf25ffb9d3b3eece2efd1a210abe0ed92b6
ewertonnunes <ewerton.florencio@outlook.com>
2022-03-01 19:29:52 -0300

    aew

--- functions/content.c
+++ functions/content.c
@@ -7,7 +7,7 @@ int	main()
 	int size = atoi(string);
 	char word[size + 1];
 	fgets( word, size + 1, stdin );
-    fgets( string, size + 1, stdin );
+  fgets( string, size + 1, stdin );
 	printf("Content-type: text/html\n\n" );
 	printf("<html>\n");
 	printf("\t<head>\n");
@@ -18,10 +18,10 @@ int	main()
 
 	printf("\t\t<h1>Resultado!</h1>\n");
 	printf("\t\t<h1>Word é: %s</h1>\n", word);
-    printf("\t\t<h1>String é: %s</h1>\n", string);
+  printf("\t\t<h1>String é: %s</h1>\n", string);
 
 	printf("\t</body>\n");
-    printf("</html>\n");
+  printf("</html>\n");
 	printf("\n\n");
 
 	return 0;
--- functions/example.c
+++ functions/example.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
 	char *string = getenv( "CONTENT_LENGTH" );
-    char *base;
+  char *base;
 	int size = atoi(string);
 	char word[size + 1];
 
-    base = (char *)malloc(size * sizeof(char));
+  base = (char *)malloc(size * sizeof(char));
 
 	fgets( word, size + 1, stdin );
-    fgets( string, size + 1, stdin );
-    fgets( base, size + 1, stdin );
+  fgets( string, size + 1, stdin );
+  fgets( base, size + 1, stdin );
 
 	printf("Content-type: text/html\n\n" );
 	printf("<html>\n");
@@ -24,12 +24,12 @@ int main()
 
 	printf("\t\t<h1>Resultado!</h1>\n");
 	printf("\t\t<h1>Word é: %s</h1>\n", word);
-    printf("\t\t<h1>String é: %s</h1>\n", string);
-    printf("\t\t<h1>Base é: %s</h1>\n", base);
+  printf("\t\t<h1>String é: %s</h1>\n", string);
+  printf("\t\t<h1>Base é: %s</h1>\n", base);
 
 	printf("\t</body>\n");
-    printf("</html>\n");
+  printf("</html>\n");
 	printf("\n\n");
-    free(base);
+  free(base);
 	return 0;
 }
